513f5cd09dfee4b6457e08bbe02f22db5aba0928
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-02-28 14:15:33 -0600

    NavBar functionality added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,21 @@ import Nav from "../Nav";
 
 function Header() {
   return (
-    <header>
-      <h1>Clint Strange</h1>
+    <header className="navbar navbar-expand-lg navbar-light bg-light">
+      <h1 className="navbar-brand">
+        <a href="#about">Clint Strange</a>
+      </h1>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
       <Nav />
     </header>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,19 +2,27 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="#about">About me</a>
+    <nav className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item mx-4">
+          <a className="nav-link" href="#about">
+            About me
+          </a>
         </li>
-        <li>
-          <span>Portfolio</span>
+        <li className="nav-item mx-4">
+          <a className="nav-link" href="/">
+            Portfolio
+          </a>
         </li>
-        <li>
-          <span>Contact me</span>
+        <li className="nav-item mx-4">
+          <a className="nav-link" href="/">
+            Contact me
+          </a>
         </li>
-        <li>
-          <span>Resume</span>
+        <li className="nav-item mx-4">
+          <a className="nav-link" href="/">
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
