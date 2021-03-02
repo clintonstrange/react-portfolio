564d272bf0dbeb603d96dbdcd9371a8a74ba5dd6
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-03-01 19:22:52 -0600

    fixed targets in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <div className="d-flex justify-content-center">
-      <a href="https://stackoverflow.com/users/14321581/clintonstrange">
+      <a
+        href="https://stackoverflow.com/users/14321581/clintonstrange"
+        target="blank"
+      >
         <span className="foot-icon fab fa-stack-overflow display-4 m-4"></span>
       </a>
-      <a href="https://github.com/clintonstrange">
+      <a href="https://github.com/clintonstrange" target="blank">
         <span className="foot-icon fab fa-github display-4 m-4"></span>
       </a>
       <a href="https://www.linkedin.com/in/clint-strange/" target="blank">
