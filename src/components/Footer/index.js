48703e89d6e170f8ca551a997017969627079e81
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-03-01 19:28:53 -0600

    footer targets

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,14 +5,14 @@ function Footer() {
     <div className="d-flex justify-content-center">
       <a
         href="https://stackoverflow.com/users/14321581/clintonstrange"
-        target="blank"
+        target="_blank"
       >
         <span className="foot-icon fab fa-stack-overflow display-4 m-4"></span>
       </a>
-      <a href="https://github.com/clintonstrange" target="blank">
+      <a href="https://github.com/clintonstrange" target="_blank">
         <span className="foot-icon fab fa-github display-4 m-4"></span>
       </a>
-      <a href="https://www.linkedin.com/in/clint-strange/" target="blank">
+      <a href="https://www.linkedin.com/in/clint-strange/" target="_blank">
         <span className="foot-icon fab fa-linkedin display-4 m-4"></span>
       </a>
     </div>
