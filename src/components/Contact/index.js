e79fe9743cd8761063baf84a747b023ebd0ea8f2
clintonstrange <71712425+clintonstrange@users.noreply.github.com>
2021-05-06 16:20:17 -0500

    added email address to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -40,7 +40,7 @@ function Contact() {
     <div className="d-flex justify-content-center">
       <section id="contact" className="m-3 p-4">
         <h1 className="text-center" data-testid="h1tag">
-        <a className="font-color" href="mailto: clintonstrange@hotmail.com">Contact Me</a>
+        <a className="font-color" href="mailto: clintonstrange@hotmail.com">Contact Me @ clintonstrange@hotmail.com</a>
         </h1>
         {/* <form
           className="container form-group"
